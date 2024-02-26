c08232d64beb4a5e0dfeb589ae26adead595609e
Sara-Koni <57260633+Sara-Koni@users.noreply.github.com>
2024-02-26 07:40:07 +0100

    Add files via upload

--- Order Manager/main.py
+++ Order Manager/main.py
@@ -1,1 +1,23 @@
+from order_manager import OrderManager
+from dotenv import load_dotenv
+import os
+
+# load environment variables from .env file
+load_dotenv()
+
+
+if __name__ == "__main__":
+    mqtt_broker_url = os.getenv("HYVE_MQTT_URL")
+    mqtt_username = os.getenv("HYVE_MQTT_USR")
+    mqtt_password = os.getenv("HYVE_MQTT_PWD")
+    heuristics_file = "heuristics.txt"
+    
+   
+    
+    with open(heuristics_file, 'r') as file:
+        heuristics = file.readlines()
+        
+    order_manager = OrderManager(mqtt_broker_url, mqtt_username, mqtt_password,heuristics)
+    # process heuristics concurrently
+    order_manager.process_heuristics(heuristics)
 
