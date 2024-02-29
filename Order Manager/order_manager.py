e51272b0efa3ceb0327ab7641652a49e815c1148
Sara-Koni <57260633+Sara-Koni@users.noreply.github.com>
2024-03-01 00:26:15 +0100

    Implement MQTT Protocol to Order Manager

--- Order Manager/order_manager.py
+++ Order Manager/order_manager.py
@@ -13,8 +13,8 @@ class OrderManager:
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_publish = self.on_publish
-        self.client.on_message = self.on_message  # Added on_message callback
-        self.client.on_subscribe = self.on_subscribe  # Added on_subscribe callback
+        self.client.on_message = self.on_message  # added on_message callback
+        self.client.on_subscribe = self.on_subscribe  # added on_subscribe callback
 
         # enable TLS for secure connection
         self.client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
@@ -26,11 +26,9 @@ class OrderManager:
         self.client.connect(mqtt_broker_url, 8883)
 
         # subscribe to topic
-        self.client.subscribe("order_manager/transportation/orders")
+        self.client.subscribe("order_manager/transportation/orders", qos = 2)
         
         self.heuristics = heuristics
-        # initialize order_id_counter
-        self.order_id_counter = 1
 
     def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
@@ -48,29 +46,25 @@ class OrderManager:
 
         for idx, heuristic in enumerate(self.heuristics):
             # initialize OrderManager with the current heuristic
-            order_instance = Order(heuristic,self.order_id_counter)
+            order_instance = Order(heuristic)
 
             # create orders from the current heuristic
             order_instance.create_orders_from_heuristics()
-            
-            # increment order_id_counter for the next heuristic
-            self.order_id_counter += 1
 
             # start a new thread to send orders periodically for the current heuristic
             thread = threading.Thread(target=self.send_order_periodically,args=(order_instance,))
             thread.start()
     
     def send_order(self, order):
-        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         # convert the order instance to a dictionary
         order_dict = order.to_dict()
         # convert the order dictionary to a JSON string
         order_json = json.dumps(order_dict)
         
         # publish the order to the MQTT broker
-        topic = f"order_manager/transportation/orders/{order.order_id_counter}"
-        self.client.publish(topic, order_json)
-        print(f"{current_time}:Order sent:", order_json)
+        topic = f"order_manager/transportation/orders/{order.order_id}"
+        self.client.publish(topic, order_json, qos = 2)
+        print(Order sent:", order_json)
 
     def send_order_periodically(self, order_instance):
       while True:
