5bfbbcd48751428087f37d8a933d8da0896f610b
Sara-Koni <57260633+Sara-Koni@users.noreply.github.com>
2024-03-01 00:36:28 +0100

    Update main.py

--- Order Manager/main.py
+++ Order Manager/main.py
@@ -10,14 +10,9 @@ if __name__ == "__main__":
     mqtt_broker_url = os.getenv("HYVE_MQTT_URL")
     mqtt_username = os.getenv("HYVE_MQTT_USR")
     mqtt_password = os.getenv("HYVE_MQTT_PWD")
-    heuristics_file = "heuristics.txt"
-    
-   
-    
-    with open(heuristics_file, 'r') as file:
-        heuristics = file.readlines()
+    heuristics_file = "heuristics.csv"
         
-    order_manager = OrderManager(mqtt_broker_url, mqtt_username, mqtt_password,heuristics)
+    order_manager = OrderManager(mqtt_broker_url, mqtt_username, mqtt_password,heuristics_file)
     # process heuristics concurrently
-    order_manager.process_heuristics(heuristics)
+    order_manager.process_heuristics()
 
