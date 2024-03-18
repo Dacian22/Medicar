7c6416da5bd0617f9f949b92e3655495160f21bb
p-koenig <45401982+p-koenig@users.noreply.github.com>
2024-03-18 12:49:19 +0200

    ADDED car icon to jpeg

--- Simulation/Routing.py
+++ Simulation/Routing.py
@@ -243,7 +243,7 @@ class Routing():  # singleton class. Do not create more than one object of this 
                 # print("vehicle: " + vehicle)
                 # Create marker for vehicle using the car icon at the current vehicle position
                 folium.Marker(location=[float(self.vehicles[vehicle]["position"][0]), float(self.vehicles[vehicle]["position"][1])],
-                              icon=folium.features.CustomIcon('Car Icon.jpeg', icon_size=(30, 30)), popup=f"Vehicle: {vehicle}").add_to(m)
+                              icon=folium.features.CustomIcon(os.getenv("CAR_ICON_FILE"), icon_size=(30, 30)), popup=f"Vehicle: {vehicle}").add_to(m)
             # plot the graph on the map
             map = ox.plot_graph_folium(G, graph_map=m, color="grey", popup_attribute="osmid", edge_width=4)
             # save the map
