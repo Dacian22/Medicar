68322f26e19c1e91bf4e1824ee86310789aa1334
Dacian22 <balintdacian@yahoo.com>
2024-03-19 15:31:31 +0200

    UPDATE Code for the LLM in Playground_LLM_Dacian.py so that only the prompt given by the user appears in LangSmith ADDED the Evaluation file for the FewShotLLM for the first 20 tests

--- Simulation/Playground_LLM_Dacian.py
+++ Simulation/Playground_LLM_Dacian.py
@@ -22,24 +22,117 @@ load_dotenv()
 client=Client(api_key=os.getenv("LANGCHAIN_API_KEY"))
 
 def invoke_llm(prompt):
-    #Load the edges from the graph, the enviroment and the model
+
+    #Create the examples for FewShot
+    examples = [
+    {
+        "question": "The edge list provided is: [('N1', 'N2'), ('N2', 'N3'), ('N3', 'N4'), ('N4', 'N5'),('N3','N6')]\n Someone fell on the floor on node N2 blocking it. Please provide the affected edges.",
+        "answer": """
+Are follow up questions needed here: Yes.
+Follow up: Is the event important enough so that node N2 is not accessible anymore?
+Intermediate answer: Yes, someone falling down would make node N2 inaccessible.
+Follow up: Which edges contain node N2?
+Intermediate answer: Edges ('N1','N2'), ('N2','N3') contain node N2
+So the final answer is: List of edges that have to be removed: ('N1','N2'), ('N2','N3')
+""",
+        "reasoning":"""
+Because the event of someone falling down on the floor is important enough to block access to the node N2 so we are removing the edges that contain the node N2, those being ('N1','N2'), ('N2','N3')
+""",
+    },
+    {
+        "question": "The edge list provided is: [('N1', 'N2'), ('N2', 'N3'), ('N3', 'N4'), ('N4', 'N5'),('N3','N6')]\n Someone dropped their ice cream on the floor on node N2. Please provide the affected edges.",
+        "answer": """
+Are follow up questions needed here: Yes.
+Follow up: Is the event important enough so that node N2 is not accessible anymore?
+Intermediate answer: No, someone dropping their ice cream would not make node N2 inaccessible.
+So the final answer is: List of edges that have to be removed: []
+""",
+        "reasoning":"""
+Because the event of someone dropping their ice cream on the floor is not important enough to block access to the node N2 so we are not removing the edges that contain the node N2, so NO edges are affected
+""",
+    },
+    {
+        "question": "The edge list provided is: [('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'),('3','6')]\n Someone died on node 2. Please provide the affected edges.",
+        "answer": """
+Are follow up questions needed here: Yes.
+Follow up: Is the event important enough so that node 2 is not accessible anymore?
+Intermediate answer: Yes, someone dying would make node 2 inaccessible.
+Follow up: Which edges contain node 2?
+Intermediate answer: Edges ('1','2'), ('2','3') contain node 2
+So the final answer is: List of edges that have to be removed: ('1','2'), ('2','3')
+""",
+        "reasoning":"""
+Because the event of someone dying is important enough to block access to the node 2 so we are removing the edges that contain the node 2, those being ('1','2'), ('2','3')
+""",
+    },
+    {
+        "question": "The edge list provided is: [('A', 'B'), ('B', 'C'), ('C','A'), ('C', 'D'), ('D', 'E'), ('C','F')]\n Someone is having a heart attack on node C. Please provide the affected edges.",
+        "answer": """
+Are follow up questions needed here: Yes.
+Follow up: Is the event important enough so that node C is not accessible anymore?
+Intermediate answer: Yes, someone having a heart attack would make node C inaccessible.
+Follow up: Which edges contain node C?
+Intermediate answer: Edges ('B', 'C'), ('C','A'), ('C', 'D'), ('C','F') contain node C
+So the final answer is: List of edges that have to be removed: ('B', 'C'), ('C','A'), ('C', 'D'), ('C','F')
+""",
+        "reasoning":"""
+Because the event of someone having a heart attack is important enough to block access to the node C so we are removing the edges that contain the node C, those being ('B', 'C'), ('C','A'), ('C', 'D'), ('C','F')
+""",
+    },
+    {
+        "question": "The edge list provided is: [('A', 'B'), ('B', 'C'), ('C','A'), ('C', 'D'), ('D', 'E'), ('C','F')]\n Someone dropped their papers on node C. Please provide the affected edges.",
+        "answer": """
+Are follow up questions needed here: Yes.
+Follow up: Is the event important enough so that node C is not accessible anymore?
+Intermediate answer: No, someone dropping their papers would not make node C inaccessible.
+So the final answer is: List of edges that have to be removed: []
+""",
+        "reasoning":"""
+Because the event of someone dropping their papers is not important enough to block access to the node C so we are not removing the edges that contain the node C, so NO edges are affected.
+""",
+    },
+    {
+        "question": "The edge list provided is: [('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'),('3','6')]\n Someone is having a seisure on node 2. Please provide the affected edges.",
+        "answer": """
+Are follow up questions needed here: Yes.
+Follow up: Is the event important enough so that node 2 is not accessible anymore?
+Intermediate answer: Yes, someone dying would make node 2 inaccessible.
+Follow up: Which edges contain node 2?
+Intermediate answer: Edges ('1','2'), ('2','3') contain node 2
+So the final answer is: List of edges that have to be removed: ('1','2'), ('2','3')
+""",
+        "reasoning":"""
+Because the event of someone having a seizure is important enough to block access to the node 2 so we are removing the edges that contain the node 2, those being ('1','2'), ('2','3')
+""",
+    }
+
+]
+
+    #Load the edges
     G=load_edges()
-    model = Ollama(model="llama2")
-    #print(G)
+
+    #Create the template and model
+    example_prompt = PromptTemplate(
+    input_variables=["question", "answer"], template="Question: {question}\n{answer}")
+
+    fewshot_template = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    prefix =f"""The edge list provided is: {G} """,
+    suffix="{input} Please provide the affected edges.",
+    input_variables=["input"],
+    example_separator='\n\n\n')
+
+    model_openai= OpenAI()
 
     #Create the LLM
-    template_new=f"context: {G} \n  requirements: Do not make something up. DO NOT PROVIDE CODE! Please Provide the result!. Only use the provided edges in the context. Please provide the edges as tuples. \n question: {{question}}"
-    template_new_example=f"As a proffesional graph modeler, you're tasked with removing edges from an edge list when something happens. Requirements: Only use the Graph provided in the question. Please list the edges that need to be removed in a comma seperated list of tuples. Respect the structure of the example for your response. THIS IS JUST AN EXAMPLE DO NOT USE THIS Example of Input: The edge list provided is: [('N1', 'N2'), ('N2', 'N3'), ('N3', 'N4'), ('N4', 'N5'),('N3','N6')]\n Someone fell on the floor on node N2 blocking it. Please provide the affected edges.\n\nExample of Output:List of edges that have to be removed: ('N1','N2'), ('N2','N3')\n Reasoning: We remove only the edges that contain node N2 because it can't be accessed anymore.END OF EXAMPLE\n\n Answer this Question: The edge list provided is: {G} \n{{question}} Please provide the affected edges."   #prompt_template = PromptTemplate(input_variables=["question"], template=template_new)
-    template_new_example2=f"As a proffesional graph modeler, you're tasked with removing edges from an edge list when something happens. Requirements: Only use the Graph provided in the question. Please list the edges that need to be removed in a comma seperated list of tuples. Respect the structure of the example for your response. THIS IS JUST AN EXAMPLE DO NOT USE THIS: USER: The edge list provided is: [('N1', 'N2'), ('N2', 'N3'), ('N3', 'N4'), ('N4', 'N5'),('N3','N6')]\n USER: Someone fell on the floor on node N2 blocking it. Please provide the affected edges.\n\n YOU:List of edges that have to be removed: ('N1','N2'), ('N2','N3')\n Reasoning: We remove only the edges that contain node N2 because it can't be accessed anymore.END OF EXAMPLE\n\n Answer this Question: The edge list provided is: {G[:50]} \n{{question}} Please provide the affected edges."   #prompt_template = PromptTemplate(input_variables=["question"], template=template_new)
+    new_graph=LLMChain(prompt=fewshot_template,llm=model_openai)
 
-    prompt_template = PromptTemplate(input_variables=["question"], template=template_new_example)
-
-    new_graph=LLMChain(prompt=prompt_template,llm=model)
 
     #Create and run the prompt
-    print(f"The edge list provided is: {G[:50]} \n" + prompt)
     answer=new_graph.invoke(prompt)
 
+    #Return the output of the LLM
     return answer["text"]
 
 def try_llm(prompt):
@@ -103,7 +196,7 @@ Because the event of someone having a heart attack is important enough to block 
         "answer": """
 Are follow up questions needed here: Yes.
 Follow up: Is the event important enough so that node C is not accessible anymore?
-Intermediate answer: No, someone dropping their papers would notmake node C inaccessible.
+Intermediate answer: No, someone dropping their papers would not make node C inaccessible.
 So the final answer is: List of edges that have to be removed: []
 """,
         "reasoning":"""
@@ -129,14 +222,16 @@ Because the event of someone having a seizure is important enough to block acces
     example_prompt = PromptTemplate(
     input_variables=["question", "answer"], template="Question: {question}\n{answer}")
 
+    G=load_edges()
+
     fewshot_template = FewShotPromptTemplate(
     examples=examples,
     example_prompt=example_prompt,
-    #prefix ="""As a proffesional graph modeler, you're tasked with removing edges from an edge list when something important happens. Requirements: MAKE SURE TO ONLY use the Graph provided in the question, DO NOT use ANY other graphs. Please list the edges that need to be removed in a comma seperated list of tuples.""",
+    prefix =f"""The edge list provided is: {G} """,
     suffix="{input}",
     input_variables=["input"],
     example_separator='\n\n\n')
-    G=load_edges()
+
     #load_dotenv()
     model = Ollama(model="llama2")
     model_mistral = Ollama(model="mistral")
@@ -162,7 +257,7 @@ Because the event of someone having a seizure is important enough to block acces
 
     #Create and run the prompt
     #print(f"The edge list provided is: {G[:50]} \n" + prompt)
-    answer=new_graph.invoke(f"The edge list provided is: {G} " + prompt + ". Please provide the affected edges.")  
+    answer=new_graph.invoke(prompt + ". Please provide the affected edges.")
 
     return answer["text"]
 
@@ -222,7 +317,6 @@ def main(ref_routing):
 
 def test():
     edge_ids,tests=load_tests()
-    print(edge_ids,tests)
     try:
         f = open(os.path.join('..','Simulation','LLMFewShot.txt'),'w')
     except:
@@ -253,7 +347,7 @@ def load_tests():
     return (edge_ids,tests)
     
 if __name__ == "__main__":
-    test()
-    #main("")
+    #test()
+    main("")
 
 
