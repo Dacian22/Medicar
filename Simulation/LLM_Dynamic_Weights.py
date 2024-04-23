026c3fc2cc181ac444a1df5a8ccdd1a6e1f591c7
Dacian22 <balintdacian@yahoo.com>
2024-04-23 16:55:13 +0300

    UPDATED the Examples for the FewShot template and the context of the other templates

--- Simulation/LLM_Dynamic_Weights.py
+++ Simulation/LLM_Dynamic_Weights.py
@@ -116,30 +116,30 @@ def get_examples():
     # Because the event of someone having a seisure is important enough to block access to the node 2 so we are removing the edges that contain the node 2, those being ('1','2'), ('2','3')
     # """,
     # },
-    # {
-    #         "question": "The edge list provided is: [('A', 'B'), ('B', 'C'), ('C','A'), ('C', 'D'), ('D', 'E'), ('C','F')]\n A group of people are chatting on node F. Please provide the affected edges.",
-    #         "answer": """
-    #     Are follow up questions needed here: Yes.
-    #     Follow up: Is the event important enough so that node F is not accessible anymore?
-    #     Intermediate answer: No, people chatting would not make node F inaccessible.
-    #     So the final answer is: List of edges that have to be removed: []. True the edge is usable.
-    #     """,
-    #         "reasoning": """
-    #     Because the event of people chatting is not important enough to block access to the node F so we are not removing the edges that contain the node F, so NO edges are affected.
-    #     """,
-    # },
-    # {
-    #         "question": "The edge list provided is: [('A', 'B'), ('B', 'C'), ('C','A'), ('C', 'D'), ('D', 'E'), ('C','F')]\n A small animal crosses the pathway on node D. Please provide the affected edges.",
-    #         "answer": """
-    #         Are follow up questions needed here: Yes.
-    #         Follow up: Is the event important enough so that node D is not accessible anymore?
-    #         Intermediate answer: No, a small animal crossing the pathway would not make node D inaccessible.
-    #         So the final answer is: List of edges that have to be removed: []. True the edge is usable.
-    #         """,
-    #         "reasoning": """
-    #         Because the event of a small animal crossing the pathway is not important enough to block access to the node D so we are not removing the edges that contain the node D, so NO edges are affected.
-    #         """,
-    # },
+    {
+        "question": "At edge edge_C_D a group of people are chatting. Please provide a mandatory single value between 0 and 100 for how much is the accessibility of the edge affected.",
+        "answer": """
+    Are follow up questions needed here: Yes.
+    Follow up: How affected is the accessibility of edge edge_C_D?
+    Intermediate answer: Not at all, people chatting would not affect the accessibility of edge edge_C_D.
+    So the final answer is: The value for how affected the accessibility of edge_C_D is by the event is 0.
+    """,
+        "reasoning": """
+    Because the event of people chatting would not affect the accessibility of edge edge_C_D we are giving a very low value of 0.
+    """,
+    },
+    {
+        "question": "At edge edge_N1_N4 a medium-sized animal is on the path. Please provide a mandatory single value between 0 and 100 for how much is the accessibility of the edge affected.",
+        "answer": """
+    Are follow up questions needed here: Yes.
+    Follow up: How affected is the accessibility of edge edge_N1_N4?
+    Intermediate answer: Moderately, a medium-sized animal being on the path would moderately affect the accessibility of edge edge_N1_N4 because the transportation vehicles would have to change their speed or position to be sure that they avoid it.
+    So the final answer is: The value for how affected the accessibility of edge_N1_N4 is by the event is 50.
+    """,
+        "reasoning": """
+    Because the event of a medium-sized animal being on the path is important enough to moderately affect the accessibility of edge_N1_N4 so we are giving a moderate value of 50.
+    """,
+    },
     # {
     #         "question": "The edge list provided is: [('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'),('3','6')]\n A vehicle accident has occured on node 3. Please provide the affected edges.",
     #         "answer": """
@@ -167,28 +167,40 @@ def get_examples():
     #         """,
     # },
     {
-            "question": "At edge edge_N5_N6 a burst fire hydrant floods the path. Please provide a mandatory single value between 0 and 100 for how much is the accessibility of the edge affected.",
-            "answer": """
-            Are follow up questions needed here: Yes.
-            Follow up: How affected is the accessibility of edge edge_N5_N6?
-            Intermediate answer: Seriously, a burst fire hydrant flooding the path would seriously affect the accessibility of edge edge_N5_N6 because the transportation vehicles would be very affected by the event.
-            So the final answer is: The value for how affected the accessibility of edge_A_B is by the event is 68.
-            """,
-            "reasoning": """
-            Because the event of a burst fire hydrant flooding the path is important enough to seriously affect the accessibility of edge_N5_N6 so we are giving a high value of 68.
-            """,
+        "question": "At edge edge_N5_N6 a burst fire hydrant floods the path. Please provide a mandatory single value between 0 and 100 for how much is the accessibility of the edge affected.",
+        "answer": """
+    Are follow up questions needed here: Yes.
+    Follow up: How affected is the accessibility of edge edge_N5_N6?
+    Intermediate answer: Seriously, a burst fire hydrant flooding the path would seriously affect the accessibility of edge edge_N5_N6 because the transportation vehicles would be very affected by the event.
+    So the final answer is: The value for how affected the accessibility of edge_A_B is by the event is 68.
+    """,
+        "reasoning": """
+    Because the event of a burst fire hydrant flooding the path is important enough to seriously affect the accessibility of edge_N5_N6 so we are giving a high value of 68.
+    """,
     },
     {
-            "question": "At edge edge_3_4 a fallen tree blocks part of the path. Please provide a mandatory single value between 0 and 100 for how much is the accessibility of the edge affected.",
-            "answer": """
-            Are follow up questions needed here: Yes.
-            Follow up: How affected is the accessibility of edge edge_3_4?
-            Intermediate answer: Moderately, a fallen tree blocking part of the path would moderately affect the accessibility of edge edge_3_4 because the transportation vehicles would have to change their speed or position to be sure that they avoid it.
-            So the final answer is: The value for how affected the accessibility of edge_A_B is by the event is 40.
-            """,
-            "reasoning": """
-            Because the event of a fallen tree blocking part of the path is important enough to moderately affect the accessibility of edge_3_4 so we are giving a moderate value of 40.
-            """,
+        "question": "At edge edge_3_4 a fallen tree blocks part of the path. Please provide a mandatory single value between 0 and 100 for how much is the accessibility of the edge affected.",
+        "answer": """
+    Are follow up questions needed here: Yes.
+    Follow up: How affected is the accessibility of edge edge_3_4?
+    Intermediate answer: Moderately, a fallen tree blocking part of the path would moderately affect the accessibility of edge edge_3_4 because the transportation vehicles would have to change their speed or position to be sure that they avoid it.
+    So the final answer is: The value for how affected the accessibility of edge_3_4 is by the event is 40.
+    """,
+        "reasoning": """
+    Because the event of a fallen tree blocking part of the path is important enough to moderately affect the accessibility of edge_3_4 so we are giving a moderate value of 40.
+    """,
+    },
+    {
+        "question": "At edge edge_N3_N4 some small branches are on the ground. Please provide a mandatory single value between 0 and 100 for how much is the accessibility of the edge affected.",
+        "answer": """
+    Are follow up questions needed here: Yes.
+    Follow up: How affected is the accessibility of edge edge_N3_N4?
+    Intermediate answer: A little, some small branches are on the ground would affect the accessibility of edge edge_N3_N4 a little because the transportation vehicles would have to slow down a little.
+    So the final answer is: The value for how affected the accessibility of edge_N3_N4 is by the event is 20.
+    """,
+        "reasoning": """
+    Because the event of small branches being on the ground would affect the accessibility of edge_N3_N4 a little so we are giving a low value of 20.
+    """,
     },
     # {
     #         "question": "The edge list provided is: [('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'), ('2','6'), ('3','7')]\n A fallen fence blocks the access near node 6. Please provide the affected edges.",
@@ -302,10 +314,13 @@ def get_template_fewshot():
 
 
 def get_template_zeroshot():
-    context_zero_shot = """As a professional graph modeler, you're tasked with determining the accessibility of edges in a transportation network.
-    You must determine whether each provided edge is usable or not based on how important the obstacle given as input is.
+    context_zero_shot = """As a professional graph modeler, you're tasked with determining the accessibility of edges in a transportation network. 
+    You must determine how much was the provided edge affected based on how important the obstacle given as input is. 
+    The values are between 0-100 with 100 being the most affected, values between 0-25 are for events that affect the accessibility of the edge a little bit, 
+    values between 25-50 are for events that moderately affect the accessibility of the edge, values between 50-75 are for events that seriously affect the accessibility of the edge 
+    and values between 75-100 affect the accessibility of the edge critically. 
     Don't provide the examples in you response but base your answer on them.
-    {input} Please provide a True/False value at the end if the edge is usable, exactly like this like this: True the edge is usable or False the edge is not usable."""
+    {input} Please provide a mandatory single value between 0 and 100 for how much is the accessibility of the edge for the transportation vehicles is affected."""
 
     zeroshot_template = PromptTemplate(input_variables=["input"], template=context_zero_shot)
 
@@ -323,12 +338,13 @@ def get_template_testing_fewshot():
     fewshot_testing_template = FewShotPromptTemplate(
     examples=examples,
     example_prompt=example_prompt,
-    prefix ="""As a professional graph modeler, you're tasked with determining the accessibility of edges in a transportation network.
-    You're tasked with removing edges from an edge list when something happens that would make the edge impassable. 
-    You must determine whether each provided edge is usable or not based on how important the obstacle given as input is. Respond
-    with True if the edge is available and False if the edge is not available.""",
-    #Using a static edge
-    suffix="At edge edge_7120224687_7112240050 {input} Please provide the affected edges, and a True/False value if the edge is usable.",
+    prefix ="""As a professional graph modeler, you're tasked with determining the accessibility of edges in a transportation network. 
+    You must determine how much was the provided edge affected based on how important the obstacle given as input is. 
+    The values are between 0-100 with 100 being the most affected, values between 0-25 are for events that affect the accessibility of the edge a little bit, 
+    values between 25-50 are for events that moderately affect the accessibility of the edge, values between 50-75 are for events that seriously affect the accessibility of the edge 
+    and values between 75-100 affect the accessibility of the edge critically. 
+    Don't provide the examples in you response but base your answer on them.""",
+    suffix="At edge edge_7120224687_7112240050 {input} Please provide a mandatory single value between 0 and 100 for how much is the accessibility of the edge for the transportation vehicles is affected.",
     input_variables=["input"],
     example_separator='\n\n\n')
 
